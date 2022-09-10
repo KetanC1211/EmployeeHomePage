ed7553e9ab49655e73190e9671be36d94aa70c4d
KetanC1211 <ketanchindarkar12@gmail.com>
2022-09-10 08:43:06 +0530

    Refactored minor error

--- empHome.js
+++ empHome.js
@@ -1,12 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     employeePayrollList = getEmployeePayrollDataFromStorage();
+    console.log(employeePayrollList)
     document.querySelector(".emp-count").textContent = employeePayrollList.length;
     createInnerHtml();
     localStorage.removeItem('editEmp');
   });
 
 const getEmployeePayrollDataFromStorage = () => {
-    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem(EmployeePayrollList)) : [];
+    return localStorage.getItem('employeePayrollList') ? JSON.parse(localStorage.getItem('employeePayrollList')) : [];
 }
 
 const createInnerHtml = () => {
@@ -19,10 +20,9 @@ const createInnerHtml = () => {
         <th>Start Date</th>
         <th>Actions</th>`;
 
-    if(employeePayrollList.length == 0) return;
-    let innerHtml = `${headerHtml}`;
     let employeePayrollDataList = createEmployeePayrollJSON();
-
+    if(employeePayrollDataList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
     for(const employeePayrollData of employeePayrollDataList){
         innerHtml = `${innerHtml}
         <tr>
@@ -30,8 +30,7 @@ const createInnerHtml = () => {
         <img class="profile" src="${employeePayrollData._profileImage}" alt="profile_img-1"></td>
         <td>${employeePayrollData._name}</td>
         <td>${employeePayrollData._gender}</td>
-        <td>${getDepartmentHtml(employeePayrollData._department)}</td>
-        
+        <td>${getDepartmentHtml(employeePayrollData._department)}</td>      
         <td>${employeePayrollData._salary}</td>
         <td>${employeePayrollData._startDate}</td>
         <td>
@@ -40,37 +39,37 @@ const createInnerHtml = () => {
         </td>
         </tr>`;
     }
-    document.querySelector('#table-display').innerHTML = innerHtml;
+    document.querySelector('#display').innerHTML = innerHtml;
 }
 
 const createEmployeePayrollJSON = () => {
-    let employeePayrollListLocal = [
-      {
-        _name: 'Neha',
-        _gender: 'Female',
-        _department: [
-          'HR'
-        ],
-        _salary: 350000,
-        _startDate: '20 Jul 2016',
-        _note: 'Test',
-        _id: new Date().getTime(),
-        _profileImage: '/assets/profile-images/Ellipse -4.png' 
-      },
-      {
-        _name: 'Karthik',
-        _gender: 'Male',
-        _department: [
-          'Engineer'
-        ],
-        _salary: 400000,
-        _startDate: '02 Mar 2020',
-        _note: 'Test to JSON',
-        _id: new Date().getTime(),
-        _profileImage: '/assets/profile-images/Ellipse -3.png'
-      }
-    ];
-    return employeePayrollListLocal;
+    // let employeePayrollListLocal = [
+    //   {
+    //     _name: 'Neha',
+    //     _gender: 'Female',
+    //     _department: [
+    //       'HR'
+    //     ],
+    //     _salary: 350000,
+    //     _startDate: '20 Jul 2016',
+    //     _note: 'Test',
+    //     _id: new Date().getTime(),
+    //     _profileImage: '/assets/profile-images/Ellipse -4.png' 
+    //   },
+    //   {
+    //     _name: 'Karthik',
+    //     _gender: 'Male',
+    //     _department: [
+    //       'Engineer'
+    //     ],
+    //     _salary: 400000,
+    //     _startDate: '02 Mar 2020',
+    //     _note: 'Test to JSON',
+    //     _id: new Date().getTime(),
+    //     _profileImage: '/assets/profile-images/Ellipse -3.png'
+    //   }
+    // ];
+    return localStorage.getItem("employeePayrollList");
 }
 const getDepartmentHtml = (departmentList) => {
     let departmentHtml = '';
@@ -78,4 +77,4 @@ const getDepartmentHtml = (departmentList) => {
       departmentHtml = `${departmentHtml} <div class='dept-label'>${department}</div>`
     }
     return departmentHtml
-  }
+}
